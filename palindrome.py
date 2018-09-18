88952f5f16b36dafb211e17fec30a96b87d6cc95
nachiyappanpudukkottaitn5518072000 <42993255+nachiyappanpudukkottaitn5518072000@users.noreply.github.com>
2018-09-18 15:59:25 +0530

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,12 @@
+n=int(input(" "))
+temp=n
+rev=0
+While(n>0):
+dig=n%10
+rev=rev*10+dig
+n=n\\10
+if(temp==rev):
+print("The number is a palindrome!")
+else:
+print("The number isn't a palindrome!")
+
