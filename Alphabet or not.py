85e9212265c59dc8ebf4c6fe6f1f99bc73bb90ab
nachiyappanpudukkottaitn5518072000 <42993255+nachiyappanpudukkottaitn5518072000@users.noreply.github.com>
2018-09-18 14:25:34 +0530

    Update Alphabet or not.py

--- Alphabet or not.py
+++ Alphabet or not.py
@@ -1,10 +1,11 @@
-while True:
-	print("Enter '0' for exit.")
-	ch = input("Enter any character: ")
-	if ch == '0':
-		break
-	else:
-		if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
-			print(ch, "is an alphabet.\n")
-		else:
-			print(ch, "is not an alphabet.\n")
+While True:
+	print("Enter '0' for exit')
+ch=input(""):
+	 break
+else:
+if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
+print(ch,"is an Alphabet.\n")
+else:
+print(ch,"is an Not.\n")	      
+      
+      
